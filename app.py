5ea592906f265ea836289f2515252f016edd320a
ajrb93 <ajbuchanan93@gmail.com>
2026-05-11 20:20:14 -0400

    20260511

--- app.py
+++ app.py
@@ -1004,7 +1004,7 @@ def plot_offdef_chart(data):
     fig.update_layout(height=105,margin=dict(l=0, r=0, t=0, b=0),yaxis=dict(range=[0, 2], tickvals=[i/10 for i in np.arange(0,21,5)], tickformat='.2f'),
                       plot_bgcolor='rgba(0,0,0,0)',paper_bgcolor='rgba(0,0,0,0)')
     season_start = '04-01'
-    season_end = '11-01'
+    season_end = '12-01'
 
     rangebreaks = []
     for year in pd.to_datetime(data.Date).dt.year.unique():
@@ -1049,8 +1049,8 @@ def plot_xg_chart(data):
         fig.add_vline(x=pd.Timestamp(f'{year}-01-01').timestamp()*1000,line_dash='dot', line_color='black', line_width=2)
     fig.update_layout(height=105,margin=dict(l=0, r=0, t=0, b=0),yaxis=dict(range=[-4,4], tickvals=list(range(-4, 5)), tickformat='.0f'),
                       plot_bgcolor='rgba(0,0,0,0)',paper_bgcolor='rgba(0,0,0,0)')
-    season_start = '02-20'
-    season_end = '11-09'
+    season_start = '04-01'
+    season_end = '12-01'
 
     rangebreaks = []
     for year in pd.to_datetime(data.Date).dt.year.unique():
